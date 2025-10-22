a45991bb4f0d229aa39743d63cfa2b2b6c49e620
JotaBarbosaDev <jotambbarbosa@gmail.com>
2025-10-22 01:17:28 +0100

    init commit BASE TEMPLATE

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,8 @@ import Logo from '@/assets/icons/Logo';
 import { Box } from '@/components/ui/box';
 import { ScrollView } from 'react-native';
 import { Text } from '@/components/ui/text';
+import {Badge, BadgeText, BadgeIcon} from "@/components/ui/badge";
+import {GlobeIcon} from "@/components/ui/icon";
 
 import { Button, ButtonText } from '@/components/ui/button';
 import { useRouter } from 'expo-router';
@@ -28,35 +30,41 @@ export default function Home() {
   const router = useRouter();
   return (
     <Box className="flex-1 bg-background-300 h-[100vh]">
-        <Box className="absolute h-[500px] w-[500px] lg:w-[700px] lg:h-[700px]">
-          <Gradient />
-        </Box>
+      <Box className="absolute h-[500px] w-[500px] lg:w-[700px] lg:h-[700px]">
+        <Gradient />
+      </Box>
       {/* <ScrollView
         style={{ height: '100%' }}
         contentContainerStyle={{ flexGrow: 1 }}
       > */}
-        <Box className="flex flex-1 items-center mx-5 lg:my-24 lg:mx-32 py-safe">
-          <Box className="gap-10 base:flex-col sm:flex-row justify-between sm:w-[80%] md:flex-1">
-            <Box className="bg-background-template py-2 px-6 rounded-full items-center flex-column md:flex-row md:self-start">
-              <Text className="text-white font-medium">
-                Get started by editing
-              </Text>
-              <Text className="text-white font-medium ml-2">./App.tsx or ./app/index.tsx (or whatever entry point you have)</Text>
-            </Box>
-            <Button
-              size="md"
-              className="bg-primary-500 px-6 py-2 rounded-full"
-              onPress={() => {
-                router.push('/tabs/tab1');
-              }}
-            >
-              <ButtonText>Explore Tab Navigation</ButtonText>
-            </Button>
+      <Box className="flex flex-1 items-center mx-5 lg:my-24 lg:mx-32 py-safe">
+        <Box className="gap-10 base:flex-col sm:flex-row justify-between sm:w-[80%] md:flex-1">
+          <Box className="bg-background-template py-2 px-6 rounded-full items-center flex-column md:flex-row md:self-start">
+            <Text className="text-white font-medium">
+              Get started by editing
+            </Text>
+            <Text className="text-white font-medium ml-2">
+              ./App.tsx or ./app/index.tsx (or whatever entry point you have)
+            </Text>
           </Box>
-          <Box className="flex-1 justify-center items-center h-[20px] w-[300px] lg:h-[160px] lg:w-[400px]">
-            <Logo />
-          </Box>
+          <Button
+            size="md"
+            className="bg-primary-500 px-6 py-2 rounded-full"
+            onPress={() => {
+              router.push("/tabs/tab1");
+            }}
+          >
+            <ButtonText>Explore Tab Navigation</ButtonText>
+          </Button>
+          <Badge size="md" variant="outline" action="error">
+            <BadgeText>Verified</BadgeText>
+            <BadgeIcon as={GlobeIcon} className="ml-2" />
+          </Badge>
         </Box>
+        <Box className="flex-1 justify-center items-center h-[20px] w-[300px] lg:h-[160px] lg:w-[400px]">
+          <Logo />
+        </Box>
+      </Box>
       {/* </ScrollView> */}
     </Box>
   );
